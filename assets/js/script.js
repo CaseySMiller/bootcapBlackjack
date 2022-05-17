b709778722e33b916e8676ddfe1d2d01278b0e9c
CaseySMiller <CaseySMiller@yahoo.com>
2022-05-17 14:10:33 -0600

    updated dealer logic and split js to work in current code base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-
+dealerCount = 0;
 // images and values are filled for testing purposes
 // we will need to populate them from the draw card function
 var faceDownCard = {cardValue: 0, img: 'http://clipart-library.com/images/8cEbeEMLi.png'};
-var dealerDrawCard = {cardValue: 10, img: 'https://deckofcardsapi.com/static/img/KH.png'};
+var dealerDrawCard = {cardValue: 6, img: 'https://deckofcardsapi.com/static/img/KH.png'};
 var dealerShowCard = {cardValue: 5, img: 'https://deckofcardsapi.com/static/img/KH.png'};
-var dealerHoleCard = {cardValue: 10, img: 'https://deckofcardsapi.com/static/img/KH.png'};
+var dealerHoleCard = {cardValue: 5, img: 'https://deckofcardsapi.com/static/img/8H.png'};
 var PlayerFirstCard = {cardValue: 10, img: 'https://deckofcardsapi.com/static/img/8H.png'};
 var PlayerSecondCard = {cardValue: 10, img: 'https://deckofcardsapi.com/static/img/8C.png'};
 
 // define elements
-var dealerCardsEl = $('#dealer-cards');
-var playerCardsEl = $('#player-cards');
+var dealerCardsEl = $('#DealerCardContainer');
+var playerCardsEl = $('#PlayerCardContainer');
 // button elements
 var buttonHit = $('#buttonHit')
 var buttonStand = $('#buttonStand')
@@ -19,35 +19,46 @@ var buttonDD = $('#buttonDD')
 var buttonShuffle = $('#buttonShuffle')
 
 
-// display dealer show card
-displayCard(dealerShowCard, dealerCardsEl);
-// display dealer hole card
-displayCard(faceDownCard, dealerCardsEl);
+
 
 // function to display a card
-function displayCard(whatCard, whereCard) {
+function displayCard(whatCard, whereCard, cardID) {
     var showThisCard = $('<img>');
     var cardDiv = $('<div>');
-    cardDiv.addClass('col-1 card');
+    cardDiv.addClass('flex-column card');
+    showThisCard.attr('id', cardID);
     showThisCard.attr('src', whatCard.img);
     whereCard.append(cardDiv);
     cardDiv.append(showThisCard);
 };
 
+//function to show dealer cards
+function displayDealerCards () {
+    // empty dealer conatianer
+    dealerCardsEl.empty();
+    // display dealer hole card
+    displayCard(faceDownCard, dealerCardsEl, 'hole-card');
+    // display dealer show card
+    displayCard(dealerShowCard, dealerCardsEl);
+};
+// comment in next line to display dealer cards
+displayDealerCards();
+
 //function for dealer to play their hand
 function dealerPlay () {
     // dealers starting count
-    var dealerCount = dealerShowCard.cardValue + dealerHoleCard.cardValue;
+    dealerCount = dealerShowCard.cardValue + dealerHoleCard.cardValue;
     var dealerStand = false;
-
+    var holeCard = $('#hole-card');
     // display dealer hole card
-    displayCard(dealerHoleCard, dealerCardsEl);
+    // displayCard(dealerHoleCard, dealerCardsEl); 
+    holeCard.attr('src', dealerHoleCard.img);
     
     // draw cards until dealer has 17 or greater
     while (dealerCount < 17) {
         // call draw card api function populates dealerDrawCard
         dealerCount = dealerCount + dealerDrawCard.cardValue;
-        displayCard(dealerHoleCard, dealerCardsEl);
+        displayCard(dealerDrawCard, dealerCardsEl);
     };
 
     // dealer busts or stands
@@ -65,7 +76,6 @@ function dealerPlay () {
 
 
 
-
 //function to handle player splitting cards
 function playerSplit() {
     console.log('player splits');
@@ -80,13 +90,28 @@ function playerSplit() {
     playingColElRight.addClass('col-4');
     playingColElRight.attr('id', 'other-hands-col');
     playerCardsEl.append(playingColElRight);
+    // append row for current hand title to left playing area column
+    var currentHandTitleRowEl = $('<div>');
+    currentHandTitleRowEl.addClass('d-flex flex-row name-plate fs-2 fw-b m-2');
+    currentHandTitleRowEl.text('Current Hand');
+    playingColElLeft.append(currentHandTitleRowEl);
     // append row for current hand to left playing area column
     var currentHandRowEl = $('<div>');
-    currentHandRowEl.addClass('row');
+    currentHandRowEl.addClass('d-flex flex-row');
     playingColElLeft.append(currentHandRowEl);
+    // append row for current hand title to left playing area column
+    var otherHandTitleRowEl = $('<div>');
+    otherHandTitleRowEl.addClass('d-flex flex-row name-plate fs-2 fw-b m-2');
+    otherHandTitleRowEl.text('Next Hands');
+    playingColElRight.append(otherHandTitleRowEl);
+    //append row for other hands
+    var otherHandsRowEl = $('<div>');
+    otherHandsRowEl.addClass('d-flex flex-row');
+    playingColElRight.append(otherHandsRowEl);
+    
     //display cards in their respective columns
     displayCard(PlayerFirstCard, currentHandRowEl);
-    displayCard(PlayerSecondCard, playingColElRight);
+    displayCard(PlayerSecondCard, otherHandsRowEl);
 
     //todo:
     //draw new card and append to correct columns
